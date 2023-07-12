53a178b4eb9ca6d2b34e2597f90f7c97879fbc91
yuu-sugai <yuu-sugai>
2023-07-12 02:55:55 +0000

    dev1ブランチにコードを追加(コミットメッセージ)
    TEST_NOTEBOOKを追加します。

---
+++ TEST_NOTEBOOK.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %run ./test_python
+
+# COMMAND ----------
+
+# MAGIC %run ./test_sql
+
+# COMMAND ----------
+
+# MAGIC %run ./test_scala
+
+# COMMAND ----------
+
+# MAGIC %run ./check
